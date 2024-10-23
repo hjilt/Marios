ecf293f7f527636eac2331bf00c457460f8e18a1
hjalteklarsen <hjla0001@stud.kea.dk>
2024-10-23 11:20:53 +0200

    Pizza getters og setters

--- src/Pizza.java
+++ src/Pizza.java
@@ -2,9 +2,34 @@ public class Pizza {
     String navn;
     double pris;
     String beskrivelse;
+
     public Pizza(String navn, double pris, String beskrivelse) {
         this.navn = navn;
         this.pris = pris;
         this.beskrivelse = beskrivelse;
+    }
+
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
+
+    public void setBeskrivelse(String beskrivelse) {
+        this.beskrivelse = beskrivelse;
+    }
+
+    public double getPris() {
+        return pris;
+    }
+
+    public void setPris(double pris) {
+        this.pris = pris;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
     }
 }
