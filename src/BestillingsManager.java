e56872cb97e8b34497c217626b815f30a307231d
djh06c <djdavidjorgen@gmail.com>
2024-10-26 16:55:03 +0200

    update?

--- src/BestillingsManager.java
+++ src/BestillingsManager.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.*;
 
 public class BestillingsManager {
     private ArrayList<Bestilling> bestillinger;
@@ -17,4 +18,16 @@ public class BestillingsManager {
 
     //lavOrdre, ordre søge funktion, fjern ordre, vis ordre, vis lavede ordre, vis ikke-færdige ordre
     //Lav en metode - søge-funktion, der returnerer en Bestilling - den skal tage parameteren ordreID.
+    public void soege(ArrayList<Bestilling> bestillinger) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Søg efter ordrenummer: ");
+        int inputTal = sc.nextInt();
+
+        if (bestillinger.contains(inputTal)) {
+            System.out.println("Ordre nr. " + inputTal + " indeholder: ");
+        } else {
+            System.out.println("Ordre nr. " + inputTal + " findes ikke... prøv igen.");
+        }
+    }
 }
