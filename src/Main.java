237a62853bd1ae720f17f48d71e1fe0a2a07bd29
hjalteklarsen <hjla0001@stud.kea.dk>
2024-10-27 14:31:24 +0100

    b

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,18 @@ public class Main {
 
         Kunde kundeEt = new Kunde("Messi", 10);
         Bestilling bestillingEt = new Bestilling(kundeEt, 1);
-
+        Kunde israfil = new Kunde("Israfil", 100);
         Menu menu = new Menu();
         Pizza hawaii = new Pizza("Hawaii", 79, "Anananananas brooo, skinke den helt gode");
         Pizza tun = new Pizza("Tun", 139, "Tun (Israfil's yndlings - han elsker det)");
+        Pizza messi = new Pizza("Legend", 79, "Legend brooo, skinke den helt gode");
 
         menu.tilfoejPizza(hawaii);
         menu.tilfoejPizza(tun);
 
+
         System.out.println("\nMario's Pizza\n");
         menu.visMenu();
-
         System.out.println("\nFjerner 'Tun' pizza fra menuem");
         menu.fjernPizzaen("Tun");
 
@@ -23,9 +24,16 @@ public class Main {
         menu.visMenu();
 
         bestillingEt.addPizza(hawaii);
-
+        bestillingEt.addPizza(messi);
+        bestillingEt.addPizza(tun);
         bestillingEt.udskrivOrdre();
         bestillingEt.lavPizza();
         bestillingEt.udskrivOrdre();
+
+        BestillingsManager bestillingsManager = new BestillingsManager();
+        bestillingsManager.lavBestilling(israfil);
+
+
+
     }
 }
