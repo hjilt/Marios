c501bdaa0078bdb96005f07ffe84369413aa1b52
djh06c <djdavidjorgen@gmail.com>
2024-10-28 13:42:54 +0100

    Update Kunde.java

--- src/Kunde.java
+++ src/Kunde.java
@@ -1,4 +1,6 @@
+import java.util.*;
 public class Kunde {
+    private static ArrayList<Kunde> kunder = new ArrayList<>();
     String navn;
     int kundeID;
     boolean erFastKunde;
@@ -6,6 +8,8 @@ public class Kunde {
     public Kunde(String navn, int kundeID) {
     this.navn = navn;
     this.kundeID = kundeID;
+    this.erFastKunde = false;
+
     }
     public String getNavn() {
         return navn;
@@ -17,6 +21,24 @@ public class Kunde {
 
     public boolean isErFastKunde() {
         return erFastKunde;
+    }
+
+    public static void tilfoejKunde(String navn) {
+        int kundeID = kunder.size() + 1;
+        Kunde nyKunde = new Kunde(navn, kundeID);
+        System.out.println("Ny kunde tilføjet: " + nyKunde.getNavn() + " med kundeID: " + nyKunde.getKundeID());
+    }
+
+    public static Kunde findKunde(int kundeID) {
+        for (Kunde kunde : kunder) {
+            if (kunde.getKundeID() == kundeID) {
+                return kunde;
+            }
+        }
+        return null;
+    }
+    @Override String toString() {
+        return "Kunde Navn: " + navn + ", kundeID: " + kundeID +", Fast Kunde: " + erFastKunde;
     }
 
 }
