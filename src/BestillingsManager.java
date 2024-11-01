96d6e04a85b4bbed64f4281db583c2660998e41c
djh06c <djdavidjorgen@gmail.com>
2024-11-01 14:27:16 +0100

    Update BestillingsManager.java

--- src/BestillingsManager.java
+++ src/BestillingsManager.java
@@ -19,15 +19,16 @@ public class BestillingsManager {
         return bestilling;
     }
 
-    //lavOrdre, ordre søge funktion, fjern ordre, vis ordre, vis lavede ordre, vis ikke-færdige ordre
     //Lav en metode - søge-funktion, der returnerer en Bestilling - den skal tage parameteren ordreID.
 
     public void findBestilling(int ordreID) {
         boolean fundet = false;
         for (Bestilling bestilling : bestillinger) {
+            //Tjekker hele bestilling arrayet igennem, for at se om der er en bestilling med matchende OrdreID
             if (ordreID == bestilling.getOrdreID()) {
                 System.out.println("Ordre nr. " + ordreID + " indeholder: ");
-                bestilling.udskrivOrdre();
+                bestilling.udskrivOrdre(); //Printer selve ordren ud, hvis den er fundet
+                //Henter udskrivnings funktionen fra klassen "Bestilling.java"
                 fundet = true;
                 break;
             }
